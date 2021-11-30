3042972f7aba2e08cef189292c7667da61eb267b
Simon Sandevik <62107622+Sandevik@users.noreply.github.com>
2021-11-30 19:04:06 +0100

    Kan nu ta emot email, telefon, webbadress

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const playwright = require("playwright");
 const fs = require("fs");
-const cheerio = require("cheerio");
+
+
 
 const browserType = "chromium";
 
@@ -12,34 +13,62 @@ const run = async function(){
     const page = await context.newPage();
     const totalSidor = 20;
     
-    const certNr, namn, arbete, ort, epost = [];
+    let certNr = ["10-538", "0-143" ];
+    let namn = [];
+    let adress= [];
+    let epost = [];
+    let temp = [];
 
-   for (let index = 1; index <= totalSidor; index++){
-     
-    let url = `https://www.sbsc.se/hitta-certifikat/#/personer/kategori/behorig-ingenjor-inbrottslarm/${index}/`
-    await page.goto(url);
-    await page.waitForLoadState("load");
-       
-    //Sparar certifieringsnummer som skall läggas till i urlen för specifika namn
-    
-    await page.waitForTimeout(200);
-    const antal = await page.$$(".cert-item .cert-item__owner a");
-
-    for await (const a of antal){
-        let certifieringsNummer = await (await (a).getAttribute("href")).slice(18);
-        let namnList = await  (a).textContent();
-        namn.push(namnList);
-        certNr.push(certifieringsNummer);
-    }
+  /* for (let index = 0; index <= totalSidor; index++){
+     //Hämta namn och certifieringsnummer
+        let url = `https://www.sbsc.se/hitta-certifikat/#/personer/kategori/behorig-ingenjor-inbrottslarm/${index}/`
+         await page.goto(url);
+         await page.waitForLoadState("load");
+          //Sparar certifieringsnummer som skall läggas till i urlen för specifika namn
+         await page.waitForTimeout(1200); // denna är viktig för att få med alla namn och cert nummer
+        
+         const antal = await page.$$(".cert-item .cert-item__owner a");
+         for await (const a of antal){
+             
+               let certifieringsNummer = await (await (a).getAttribute("href")).slice(18);
+               let namnList = await (a).textContent();
+               namn.push(namnList);
+              certNr.push(certifieringsNummer);
+              
+         }        
          
-   }
-   console.log(certNr);
+    }*/
 
+    //öppnar sidan för specifikt certifikatnummer loopar igenom alla certifikatnummer
+    for  (let a of certNr){
 
+        url = `https://www.sbsc.se/personcertifikat/${a}`
+        await page.goto(url);
+        await page.waitForLoadState("load");
+       // let mail = await page.$("body > div.off-canvas-content > section.cert-single-wrapper.space.bg-white > div > div > div > div > div > div:nth-child(3) > div > p:nth-child(10) > span:nth-child(2) > a");
+
+        let mail = await page.$$("body > div.off-canvas-content > section.cert-single-wrapper.space.bg-white > div > div > div > div > div > div:nth-child(3) > div p span a ");
+
+        for await (let b of mail) {
+            
+          console.log(await b.innerHTML());
+        }
+
+        
+
+        
+        
+        
+
+    }
+   
+    
+    browser.close();
 
     //await fs.appendFile("./html.txt",certifieringsNummer, err =>{if (err) throw err; console.log("File saved!");});  // Funkar!!! 
-    browser.close();
+    
 }
+
 
 
 
