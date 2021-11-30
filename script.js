ab223c3e573462911b8f61ff9aec996efd2e466e
Simon Sandevik <62107622+Sandevik@users.noreply.github.com>
2021-11-30 22:53:58 +0100

    Bot färdiställd och fungerar, problem är att den vid senare tilfälle kanske inte kan stämma för att certifikatslistorna inte är konsekvent

--- script.js
+++ script.js
@@ -6,20 +6,18 @@ const fs = require("fs");
 const browserType = "chromium";
 
 const run = async function(){
-
     const browser = await playwright[browserType].launch({headless:false});
-    
     const context = await browser.newContext();
     const page = await context.newPage();
     const totalSidor = 20;
-    
-    let certNr = ["10-538", "0-143" ];
+    let certNr = [];
     let namn = [];
-    let adress= [];
+    let webbsida = [];
     let epost = [];
     let temp = [];
+    let telefonNr = [];
 
-  /* for (let index = 0; index <= totalSidor; index++){
+    for (let index = 0; index <= totalSidor; index++){
      //Hämta namn och certifieringsnummer
         let url = `https://www.sbsc.se/hitta-certifikat/#/personer/kategori/behorig-ingenjor-inbrottslarm/${index}/`
          await page.goto(url);
@@ -34,39 +32,44 @@ const run = async function(){
                let namnList = await (a).textContent();
                namn.push(namnList);
               certNr.push(certifieringsNummer);
-              
          }        
-         
-    }*/
+    } 
 
     //öppnar sidan för specifikt certifikatnummer loopar igenom alla certifikatnummer
-    for  (let a of certNr){
-
+    for await (let a of certNr){
         url = `https://www.sbsc.se/personcertifikat/${a}`
         await page.goto(url);
-        await page.waitForLoadState("load");
-       // let mail = await page.$("body > div.off-canvas-content > section.cert-single-wrapper.space.bg-white > div > div > div > div > div > div:nth-child(3) > div > p:nth-child(10) > span:nth-child(2) > a");
-
+        await page.waitForLoadState();
+        await page.waitForTimeout(500);
         let mail = await page.$$("body > div.off-canvas-content > section.cert-single-wrapper.space.bg-white > div > div > div > div > div > div:nth-child(3) > div p span a ");
-
+        // pusha alla epostadresser, telefonnummer, webbsidor
         for await (let b of mail) {
-            
-          console.log(await b.innerHTML());
+          temp.push(await b.innerHTML());
         }
-
-        
-
-        
-        
-        
-
+        //lägg telefonnummer i separat array
+        for (let i = 0; i < temp.length; i+=3){
+            telefonNr.push(temp[i]);
+        }
+        //-||- med epost
+        for (let i = 1; i < temp.length; i+=3){
+            epost.push(temp[i]);
+        }
+        //-||- med webbsida
+        for (let i = 2; i < temp.length; i+=3){
+            webbsida.push(temp[i]);
+        }
     }
-   
-    
+     //ta bort förta elementet som är en kopia 
+            telefonNr.shift();
+            epost.shift();
+            webbsida.shift();
+           console.log(namn[1], epost[1], certNr[1], telefonNr[1], webbsida[1])
+    for (let index = 0; index < namn.length; index++) {
+        //sparar allt i fil
+        await fs.appendFile("./personer.txt", `${namn[index]}  ${epost[index]}  ${telefonNr[index]}  ${certNr[index]}  ${webbsida[index]}\n`, err =>{if (err) throw err; console.log("File saved!");});  
+        
+    }
     browser.close();
-
-    //await fs.appendFile("./html.txt",certifieringsNummer, err =>{if (err) throw err; console.log("File saved!");});  // Funkar!!! 
-    
 }
 
 
